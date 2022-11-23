5db7fc77aa88317ab28fa06a45958b86aeaa3679
JaehyoJJang <yshrim12@naver.com>
2022-11-23 15:46:26 +0900

    SSH KEY REGISTRATION AUTOMATION --> Update

--- 03_sshKey_registration_automation/ssh_automation.sh
+++ 03_sshKey_registration_automation/ssh_automation.sh
@@ -3,11 +3,13 @@ TARGET_HOST='user_01'
 TARGET_IP='192.168.219.167'
 PORT='9706'
 
-/usr/bin/ssh-keygen -t rsa -N '' -f "${SOURCE}/id_rsa" <<< y 1>/dev/null
+if [[ ! -f "./id_rsa.pub" ]];then
+  /usr/bin/ssh-keygen -t rsa -N '' -f "${SOURCE}/id_rsa" <<< y 1>/dev/null
+fi
 
 for x in $(ls -lh ${SOURCE} | awk '{print $9}');
 do
   if [[ ${x} == 'id_rsa.pub' ]];then
-  /usr/bin/scp -P ${PORT} "${SOURCE}/${x}" ${TARGET_HOST}@${TARGET_IP}:~/.ssh/test.pub
+  /usr/bin/scp -P ${PORT} "${SOURCE}/${x}" ${TARGET_HOST}@${TARGET_IP}:~/.ssh/id_rsa.pub
   fi
 done
