fce41c82eaf3ecc9ce222281c399f80ace41b8d9
JaehyoJJAng <yshrim12@naver.com>
2022-11-29 13:43:57 +0900

    auto branch Updated

--- 05_find_a_user/script.sh
+++
@@ -1,8 +0,0 @@
-#### Find a user ####
-
-users=($(cat /etc/passwd | awk -F':' '{print $1}'))
-
-for user in ${users[@]};
-do
-	echo ${user}
-done
--- 09_directory_check/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pwd=$(pwd)
-dir="${pwd}/arthur"
-
-if [[ -d ${dir} ]];then
-	echo "The ${dir} exists"
-else
-	echo "The ${dir} does not exists"
-fi
