4695243768d1ffb7053b237de120a8cdbd6ba726
JaehyoJJAng <yshrim12@naver.com>
2022-11-30 17:49:04 +0900

    Backup Dir Automation

---
+++ 33_backuo_automation/main.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Daily Archive - Archive designated files & directories
+################################################
+#
+
+# Gather current Date
+date=$(date +"%Y%m%d")
+
+# Set Archive File Name
+FILE=archive${date}.tar.gz
+
+# Set Configuration and Destination File
+CONFIG_FILE='./Files_To_Backup'
+DESTINATION='./backup_gz'
+
+############## Main Script ##############
+
+# Check Backup Config File exists
+if [[ -f ${CONFIG_FILE} ]]
+then
+	echo
+else
+	echo -e "\n ${CONFIG_FILE} does not exists!\n"
+	exit 1
+fi
+
+
+TARGETS=$(cat ${CONFIG_FILE})
+
+for TARGET in ${TARGETS[@]}
+do
+	if [[ -d ${TARGET} ]]
+	then
+		for item in $(ls -lh ${TARGET} | awk '{print $9}')
+		do
+			if [[ ! -e ${TARGET}/${item} ]]
+			then
+				echo "${TARGET}/${item} does not exist!"
+				exit 1
+			fi
+		done
+
+		# Backup
+		echo -e "${TARGET} Start Archiving .."
+		TMP='./tmp'
+
+		if [[ ! -d ${TMP} ]]
+		then
+			mkdir ${TMP}
+		fi
+
+		tar -czpPf "${TARGET}.tar.gz" "${TARGET}" && mv "${TARGET}.tar.gz" ${TMP}
+	fi
+done
+
+
+# Set zip Files
+echo -e "\nSetting tar Files"
+tar -czpPf "${DESTINATION}/${FILE}" ${TMP}/* || exit 1
+echo -e "Done\n"
+
+# Removing TMP
+echo -e "\nRemoving TMP DIR"
+rm -rf './tmp'
+
+
